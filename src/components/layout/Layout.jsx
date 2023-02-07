95d5279fa15e12447f3c8dbe7019073be3dc0738
JellyKingdom <jyhan1993@gmail.com>
2023-02-07 14:44:39 +0900

    App.js -> Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import './layout.css';
 
-function Layout(props) {
+const Layout = (props) => {
     return (
         <>
-        <div className="layout">
-            <div className="header">   
-                <div>My TodoList</div>
-                <div>React</div>
+            <div className="layout">
+                <div className="header">
+                    <div>My TodoList</div>
+                    <div>React</div>
+                </div>
+                <div
+                    style={{
+                        border: "1px solid red",
+                    }}
+                ></div>
             </div>
-            <div style={{
-                border: "1px solid red"
-            }}>
             {props.children}
-            </div>
-        </div>
-        
         </>
     );
-}
+};
 
 export default Layout;
