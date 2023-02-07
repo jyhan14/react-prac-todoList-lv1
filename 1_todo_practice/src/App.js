49c54844d8435c1d2a9e520a6b1d9b4e6efeb04f
JellyKingdom <jyhan1993@gmail.com>
2023-02-07 14:09:42 +0900

    feat: insert

--- 1_todo_practice/src/App.js
+++ 1_todo_practice/src/App.js
@@ -2,23 +2,27 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 1, 
-    title: "삭제기능 구현하기", 
-    body: "2/7일까지 완료해야합니다.", 
-    isDone: false},
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: "삭제기능 구현하기",
+            body: "2/7일까지 완료해야합니다.",
+            isDone: false,
+        },
 
-    {id: 2, 
-    title: "추가기능 구현하기", 
-    body: "2월 6일까지 완료해야합니다.", 
-    isDone: true}
-  ]);
+        {
+            id: 2,
+            title: "추가기능 구현하기",
+            body: "2월 6일까지 완료해야합니다.",
+            isDone: true,
+        },
+    ]);
 
     return (
         <div>
             <Layout>
-                <TodoForm todos={todos} setTodos={setTodos}/>
-                <TodoItems todos={todos} setTodos={setTodos}/>
+                <TodoForm todos={todos} setTodos={setTodos} />
+                <TodoItems todos={todos} setTodos={setTodos} />
             </Layout>
         </div>
     );
@@ -43,51 +47,96 @@ const Layout = (props) => {
     );
 };
 
-const TodoForm = ({todos, setTodos}) => {
+const TodoForm = ({ todos, setTodos }) => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    const titleChangeHandler = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const bodyChangeHandler = (e) => {
+        setBody(e.target.value);
+    };
+
+    const addTodos = (e) => {
+        e.preventDefault();
+
+        const newTodos = {
+            id: todos.length + 1,
+            title,
+            body,
+            isDone: false,
+        };
+
+        //원래있던 todos를 스프레드연산자로 풀어주고, 거기서 newTodos를 다시 넣은 것!
+        setTodos([...todos, newTodos]);
+    };
     return (
-        <form className="todoForm">
+        <form className="todoForm" onSubmit={addTodos}>
             <div className="inputs">
                 <label>제목 : </label>
-                <input type="text" />
+                <input
+                    type="text"
+                    value={title}
+                    onChange={titleChangeHandler}
+                />
                 <label>내용 : </label>
-                <input type="text" />
+                <input type="text" value={body} onChange={bodyChangeHandler} />
             </div>
-
             <button>추가하기</button>
         </form>
     );
 };
 
-const TodoItems = ({todos, setTodos}) => {
+const TodoItems = ({ todos, setTodos }) => {
     return (
         <div className="todoItems">
             <h2 className="title">Working</h2>
             <div className="itemsWrapper">
-                <div className="todoContainer">
-                    <div>
-                        <h2>{todos[0].title}</h2>
-                        <div>{todos[0].body}</div>
-                    </div>
-
-                    <div className="btns">
-                        <button>삭제</button>
-                        <button>취소 or 완료</button>
-                    </div>
-                </div>
+                {todos.map(function (todo) {
+                    if (!todo.isDone) {
+                        return (
+                            <Todo
+                                key={todo.id}
+                                todos={todos}
+                                setTodos={setTodos}
+                                todo={todo}
+                            />
+                        );
+                    }
+                })}
             </div>
             <h2 className="title">Done</h2>
             <div className="itemsWrapper">
-                <div className="todoContainer">
-                    <div>
-                        <h2>{todos[1].title}</h2>
-                        <div>{todos[1].body}</div>
-                    </div>
+                {todos.map(function (todo) {
+                    if (todo.isDone) {
+                        return (
+                            <Todo
+                                key={todo.id}
+                                todos={todos}
+                                setTodos={setTodos}
+                                todo={todo}
+                            />
+                        );
+                    } else return null;
+                })}
+            </div>
+        </div>
+    );
+};
 
-                    <div className="btns">
-                        <button>삭제</button>
-                        <button>취소 or 완료</button>
-                    </div>
-                </div>
+const Todo = ({ todo }) => {
+    return (
+        <div className="todoContainer">
+            <div>
+                <h2>{todo.title}</h2>
+                <div>{todo.body}</div>
+            </div>
+
+            <div className="btns">
+                <button>삭제</button>
+                <button>{todo.isDone ? "취소" : "완료"}</button>
             </div>
         </div>
     );
