26ad66ace76b0999c0666d11b9553f66ae84558e
JellyKingdom <jyhan1993@gmail.com>
2023-02-07 14:43:40 +0900

    App.js -> TodoList.jsx

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
 import Layout from "../components/layout/Layout";
 import TodoForm from "../components/todoForm/TodoForm";
-import TodoItems from "../components/todoItems/TodoItems";
+import TodoItems from "../components/TodoItems";
 
 const TodoList = () => {
     const [todos, setTodos] = useState([
         {
             id: 1,
-            title: "리액트 공부하기",
-            body: "리액트 기초를 공부해봅시다.",
+            title: "삭제기능 구현하기",
+            body: "2/7일까지 완료해야합니다.",
             isDone: false,
         },
+
         {
             id: 2,
-            title: "완료 test",
-            body: "isDone에 true 넣어보기!",
+            title: "추가기능 구현하기",
+            body: "2월 6일까지 완료해야합니다.",
             isDone: true,
         },
     ]);
+
     return (
-        <>
+        <div>
             <Layout>
                 <TodoForm todos={todos} setTodos={setTodos} />
                 <TodoItems todos={todos} setTodos={setTodos} />
             </Layout>
-        </>
+        </div>
     );
 };
 
